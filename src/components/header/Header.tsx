0e5b50cf2e953b90200092f24d991c35a443b46a
indagz <indira.agzamova@ps.kz>
2026-03-29 17:58:57 +0500

    fix: removed container mixin

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,23 +7,28 @@ import { MIN_WIDTH_TABLET } from '@/shared/constants';
 import HeaderMobileDrawer from './HeaderMobileDrawer';
 import HeaderSectionsLinks from './HeaderSectionsLinks';
 import HeaderLocaleButtons from './HeaderLocaleButtons';
+import { Container } from '../container/Container';
 
 const Header = () => {
   const width = useViewport();
 
   return (
     <header className={styles.header}>
-      <HeaderLogo />
-      {width !== null && width <= MIN_WIDTH_TABLET ? (
-        <HeaderMobileDrawer />
-      ) : (
-        <nav className={styles.navigation} aria-label="Main navigation">
-          <ul className={styles.navigation_list}>
-            <HeaderSectionsLinks excludeMain />
-            <HeaderLocaleButtons />
-          </ul>
-        </nav>
-      )}
+      <Container>
+        <div className={styles.header_inner}>
+          <HeaderLogo />
+          {width !== null && width <= MIN_WIDTH_TABLET ? (
+            <HeaderMobileDrawer />
+          ) : (
+            <nav className={styles.navigation} aria-label="Main navigation">
+              <ul className={styles.navigation_list}>
+                <HeaderSectionsLinks excludeMain />
+                <HeaderLocaleButtons />
+              </ul>
+            </nav>
+          )}
+        </div>
+      </Container>
     </header>
   );
 };
