a3128b8f8ade90f42bfc506e9820b03ddba0efd3
indagz <indira.agzamova@ps.kz>
2026-03-29 05:16:21 +0500

    chore: added demo in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import { useState } from 'react';
 import FeedbackModal from '@/components/feedback-modal/FeedbackModal';
 import ButtonMain from '@/components/button-main/ButtonMain';
 import Footer from '@/components/footer/Footer';
+import Header from '@/components/header/Header';
 
 const Home = () => {
   const [isFeedbackOpen, setIsFeedbackOpen] = useState(false);
 
   return (
     <main>
+      <Header />
       <button type="button" onClick={() => setIsFeedbackOpen(true)}>
         Open feedback
       </button>
